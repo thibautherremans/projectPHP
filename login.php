66f8986d26c491de4053bd4b1e4b3bcdd7b7a260
rubenrooms <ruben_rooms@hotmail.com>
2021-03-28 15:25:19 +0200

    add: style to loginpage using bootstrap

--- login.php
+++ login.php
@@ -25,21 +25,41 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
     <title>login page</title>
 </head>
-<body>
+<body class="text-center">
 
     //include van navigatie
 
-    <header class="login__head">
-        //logo + catchfrase
+    <header class="login__head m-auto">
+        <img src="/images/logo-insta.png" alt="" class="mb-4" width="70" height="70">
+        <h1 class="h3 mb-3 fw-normal">Please login</h1>
     </header>
 
-    <main>
+    <main class="form-login m-auto">
         <form action="" method="post" class="login__form">
-            <input type="text" placeholder="email" name="email">
-            <input type="password" placeholder="password" name="password">
-            <input type="submit" placeholder="log-in" name="submit">
+            
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="email" name="email">
+                <label for="floatingInput">Email address</label>
+            </div>
+            
+            <div class="form-floating pb-3">
+            <label for="floatingPassword">Password</label>
+                <input type="password" placeholder="password" id="floatingPassword" name="password" class="form-control">
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+
+            <button class="w-50 btn btn-lg btn-primary" type="submit" name="login">Log in</button>
         </form>
 
         <a href="register.php">make an account</a>
