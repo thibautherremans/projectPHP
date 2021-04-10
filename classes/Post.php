629bd81dc9baf2c0d24a17b94fb77752af1ae311
victor massaer <victor@massaer.be>
2021-04-10 13:28:55 +0200

    working on upload/post

---
+++ classes/Post.php
@@ -0,0 +1,58 @@
+<?php
+    include_once(__DIR__ . "../database/Db.php");
+
+    class Post{
+        private $image;
+        private $description;
+
+        public function getDescription()
+        {
+            return $this->description;
+        }
+
+        public function setDescription($description): void
+        {
+            if($description != null){
+                $this->description = $description;
+            }else{
+                throw new Exception("description can not be empty");
+            }
+
+        }
+
+        public function getImage()
+        {
+            return $this->image;
+        }
+
+        public function setImage($image)
+        {
+            //controleren op grootte
+            $check = getimagesize($image);
+            if($check != false){
+
+            }else{
+                echo("mislukt");
+                throw new Exception("image not uploaded correctly");
+            }
+            //controleren op bestandstype
+
+            //downscalen naar kleinere grootte
+
+
+        }
+
+        function UploadImage($image){
+
+                $conn  = new PDO('mysql:host=localhost;dbname=technodb', "root", "root");
+                $statement = $conn->prepare("insert into post where (mediafile) = (:image)");
+                $statement->bindValue(":image", $image);
+                $statement->execute();
+                $result = $statement->fetch();
+
+
+        }
+
+
+
+    }
--- upload.php
+++ upload.php
@@ -1,22 +1,37 @@
-<!doctype html>
+<?php
+    include_once(__DIR__ . "/classes/Post.php");
+
+    if(!empty($_POST)){
+        $post = new Post();
+        //$post->UploadImage($_POST["UploadImage"]);
+        $post->setImage($_POST["uploadImage"]);
+        $post->setDescription($_POST["description"]);
+        var_dump($_POST["UploadImage"]);
+    }
+?><!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <?php include_once("nav.inc.php");?>
 
-    //select a picture
+    <form action="" method="post" enctype="multipart/form-data">
+        //select a picture
+        <input type="file" name="uploadImage" id="uploadImage">
+        <input type="text" name="description" id="description">
+        <input type="submit" value="Upload Image" name="submit" id="submit">
+        //add a description to the picture
 
-    //add a description to the picture
+        //tags like #breakfast must redirect to other pictures with this tag
 
-    //tags like #breakfast must redirect to other pictures with this tag
-
-    //upload the picture to the feed/profile (reduce size and quality first)
+        //upload the picture to the feed/profile (reduce size and quality first)
+    </form>
 
 </body>
 </html>
