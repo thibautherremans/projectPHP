0ffbab9d1039cbf22bca917b896eb328f7c99ae2
victor massaer <victor@massaer.be>
2021-05-24 12:57:38 +0200

    feat: add style to profile page

--- profile.php
+++ profile.php
@@ -34,32 +34,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style/profile.css">
     <title>Profile</title>
 </head>
 <body>
     <?php include_once("nav.inc.php");?>
 
     <section class="info">
-        <img src="<?php echo $profilepicture; ?>" alt="profile picture">
+        <img src="<?php echo $profilepicture; ?>" alt="profile picture" id="profileProfilepicture">
         <h2><?php echo htmlspecialchars($name); ?></h2>
         <h3><?php echo htmlspecialchars($email); ?></h3>
         <p><?php echo htmlspecialchars($description); ?></p>
+
+        <section class="follow">
+            <input type="submit" value="follow this user" class="btnFollow" data-userid="<?php echo $id ?>">
+        </section>
+
+        <?php if($_SESSION['id'] == $id): ?>
+            <a href="editProfile.php" id="btnEdit">edit profile</a>
+        <?php endif; ?>
     </section>
 
-    <section class="follow">
-        <input type="submit" value="follow this user" class="btnFollow" data-userid="<?php echo $id ?>">
-    </section>
 
-    <?php if($_SESSION['id'] == $id): ?>
-        <a href="editProfile.php">edit profile</a>
-    <?php endif; ?>
 
     <?php foreach($posts as $p): ?>
         <?php if (++$i == 21) break; ?>
-        <section class="post_<?php echo $p["id"]?>">
+        <section class="post_<?php echo $p["id"]?>" id="profilePost">
 
-            <img src="uploads/<?php echo $p["imagePath"];?>" alt="<?php echo $p["imagePath"];?>">
+            <img src="uploads/<?php echo $p["imagePath"];?>" alt="<?php echo $p["imagePath"];?>" id="profilePostPicture">
             <p><?php echo $p["description"];?></p>
 
             <?php
@@ -73,7 +75,7 @@
             </form>
 
             <form action="" method="post">
-                <input class="commentText_<?php echo $p['id']; ?>" type="text" name="comment" placeholder="place comment">
+                <input class="commentText_<?php echo $p['id']; ?>" type="text" name="comment" placeholder="place comment" id="postInputComment">
                 <a href="#" class="btnAddComment" data-postid= "<?php echo $p['id'];?>">add comment</a>
             </form>
 
@@ -81,7 +83,7 @@
                 $comment = new Comment;
                 $comments =  $comment->loadComments($p['id']);
                 foreach($comments as $c): ?>
-            <ul class="commentList_<?php echo $p['id']; ?>">
+            <ul class="commentList_<?php echo $p['id']; ?>" id="postCommentList">
                 <li><?php echo htmlspecialchars($c["message"]) ; ?> <?php echo $c["post_date"]?></li>
             </ul>
             <?php endforeach; ?>
